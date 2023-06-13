13ca43845603a9df0fc4b46f9a596680726da8ab
Quintin Mason <122917536+Qballzzz@users.noreply.github.com>
2023-06-13 13:03:04 -0600

    Delete BackEnd_AutomaticTaskSchduler-main directory

--- BackEnd_AutomaticTaskSchduler-main/AutoTask/src/main/java/com/AutoTask/AutoTask/repository/TaskRepository.java
+++
@@ -1,9 +0,0 @@
-package com.AutoTask.AutoTask.repository;
-
-import com.AutoTask.AutoTask.models.Task;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface TaskRepository extends JpaRepository<Task, Integer> {
-}
